16f2f95c5bb2afb50f13dc19ff6fdc9a3269a444
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-13 20:43:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,6 +109,7 @@ def changing_alan
         }
      }
      programmer_hash[:alan_kay][:known_for]="GUI"
+     return programmer_hash
      
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
